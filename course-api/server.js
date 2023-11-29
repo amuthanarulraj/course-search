864692d855c7216a734e6520839400b24f6ef843
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2023-11-28 23:02:24 -0500

    Updating the api port

--- course-api/server.js
+++ course-api/server.js
@@ -2,6 +2,6 @@ import express from 'express';
 import initialize from './app/app.js';
 
 const app = express();
-const port = 3000;
+const port = 3002;
 initialize(app);
 app.listen(port, () => console.log(`Server is listening at port ${port}`));
