234ecf48986dff63d3aea99a409a67ba96dd7a20
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2023-11-28 23:14:06 -0500

    Adding environment

--- course-api/app/app.js
+++ course-api/app/app.js
@@ -8,7 +8,7 @@ const initialize = (app) => {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded());
-    mongoose.connect('mongodb_connection_url');
+    mongoose.connect(process.env.MONGO_CONNECTION);
     registerRouter(app);
 }
 
--- course-api/server.js
+++ course-api/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import initialize from './app/app.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = 3002;
+const port = process.env.SERVER_PORT;
 initialize(app);
 app.listen(port, () => console.log(`Server is listening at port ${port}`));
