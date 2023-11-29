2b89f8454099f551d73a8f50ef1c7284b9e1bf49
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2023-11-28 23:19:20 -0500

    Updating store files

--- course-app/src/store/index.ts
+++ course-app/src/store/index.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import coursesReducer from './slices/course-slice';
+import coursesReducer from './slices/course-slice'
 
  export const store = configureStore({
     reducer: {
-        courses: coursesReducer
+        [coursesReducer.name]: coursesReducer
     }
  });
